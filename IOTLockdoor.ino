ae97063a1c63b3e0af925010bc1e478c4acba60a
Unknown <a.hosseinsalehi@gmail.com>
2018-05-31 16:55:43 +0430

    Amir: save status in server

--- IOTLockdoor.ino
+++ IOTLockdoor.ino
@@ -2,11 +2,18 @@
 #include <ESP8266WiFi.h>
 #include <ArduinoJson.h>
 #include <ESP8266WebServer.h>
+#include <SimpleTimer.h>
 
 // WiFi Definitions
 const char* _ssid = "Lock Wifi 2";
 const char* password_serial_number = "123456789"; // has to be longer than 7 chars
 HTTPClient httpSubscriber;
+SimpleTimer timer;
+bool dataready = false;
+String thisString;
+String messageId_data = "0";
+String LastmessageId_data = "1";
+int httpCode;
 int i = 0;
 int val = 0;
 int last_val=HIGH;
@@ -26,8 +33,9 @@ struct tys_Status{
 #define MotorPin1 12
 #define MotorPin2 14
 
-
 tys_Status DoorData;
+tys_Status LastDoorData;
+uint8_t Status_RetValue = 0;
 ESP8266WebServer server(80);
 
 /*----------------------------------------------------------------------*/
@@ -50,10 +58,12 @@ void setup(){
    pinMode(MotorEncoder2,INPUT);
    
    pinMode(reedSwitch_Pin, INPUT);
-   delay(100);
+   
    WiFi.mode(WIFI_AP_STA);
    WiFi.softAP(_ssid, password_serial_number, 1, 0);
-   delay(100);
+
+   timer.setInterval(20000,setSubscriber);
+   timer.setInterval(1000,send_ChangeStatus);
    server.on("/", handleRoot);
    server.on("/connect", handleConnect);
    server.on("/disconnect", handledisConnect);
@@ -82,7 +92,7 @@ void setup(){
     
    server.onNotFound(handleNotFound); 
    server.begin(); //Start the server
-   delay(100);
+   
    //Serial.println("Server listening");
    //setSubscriber();
 }
@@ -94,6 +104,8 @@ void loop() {
     server.handleClient();
     ESP.wdtFeed();
     
+    timer.run();
+    
     val = digitalRead(MotorEncoder2);
     if(val != last_val){
       if(val == LOW){
@@ -104,6 +116,9 @@ void loop() {
     last_val = val;
     if(doorDirection == 1){
       if(TurnCounter >= 4){
+        digitalWrite(MotorPin1, HIGH);
+        digitalWrite(MotorPin2, HIGH);
+        delay(20);
         digitalWrite(MotorPin1, LOW);
         digitalWrite(MotorPin2, LOW);
         TurnCounter =0;
@@ -112,48 +127,23 @@ void loop() {
     }
     else{
       if(TurnCounter >= 4){
+        digitalWrite(MotorPin1, HIGH);
+        digitalWrite(MotorPin2, HIGH);
+        delay(20);
         digitalWrite(MotorPin1, LOW);
         digitalWrite(MotorPin2, LOW);
         TurnCounter =0;
         Serial.println("motorstop");
       }
     }
-//  int httpCode;
-//  httpCode = httpSubscriber.GET();
-//  String thisString = httpSubscriber.getString();
-//  delay(1000);
-//    if (httpCode == 200 && thisString != "{\"messages\":[]}")
-//    {
-//      const size_t bufferSize = JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(1) + JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(5) + 310;
-//      DynamicJsonBuffer jsonBuffer(bufferSize);
-//      
-//      char jsonValue[sizeof(thisString)];
-//      thisString.toCharArray(jsonValue, sizeof(jsonValue));
-//      
-//      JsonObject& root = jsonBuffer.parseObject(thisString);
-//            
-//      JsonObject& messages0 = root["messages"][0];
-//
-//      String messages0_data = messages0["data"];
-//      
-//          if (messages0_data == "toggle")
-//        {
-//          if(doorDirection == 0){
-//          doorDirection = 1;
-//    
-//          digitalWrite(MotorPin1, LOW);
-//          digitalWrite(MotorPin2, HIGH);
-//          handlepublish();
-//          }
-//          else{
-//          doorDirection = 0;
-//          
-//          digitalWrite(MotorPin1, HIGH);
-//          digitalWrite(MotorPin2, LOW);
-//          handlepublish();
-//          }
-//        }
-//    }
+
+    if(dataready == true && LastmessageId_data != messageId_data){
+      handle_DataParser();
+       Serial.println("data is parsing....");
+       LastmessageId_data = messageId_data;
+      dataready = false;
+    }
+    
 }
 
 /*----------------------------------------------------------------------*/
@@ -168,8 +158,6 @@ void handleRoot() {
 /*----------------------------------------------------------------------*/
 void handleConnect() {
   if (server.hasArg("needed_ssid") == true){
-
-
     WiFi.begin((server.arg("needed_ssid")).c_str(), (server.arg("needed_password")).c_str());
 
     Serial.println((server.arg("needed_ssid")).c_str());
@@ -321,7 +309,7 @@ void handleStop(){
 }
 
 /*----------------------------------------------------------------------*/
-/*                              handle toggle                             */
+/*                              handle toggle                           */
 /*----------------------------------------------------------------------*/
 void handleToggle(){
   if(doorDirection == 0){
@@ -336,7 +324,6 @@ void handleToggle(){
     digitalWrite(MotorPin2, LOW);
   }
   server.send(200, "text/plain", "200: Ok");
-  handleSaveStatusInServer();
 }
 /*----------------------------------------------------------------------*/
 /*                        handlegetAuthentication                       */
@@ -356,21 +343,21 @@ void handleSignalStrenght() {
 }
 
 /*----------------------------------------------------------------------*/
-/*                                Door Status                                */
+/*                                Door Status                           */
 /*----------------------------------------------------------------------*/
 void handleDoorStatus(){
   digitalWrite(LED_BUILTIN, HIGH);
 }
 
 /*----------------------------------------------------------------------*/
-/*                                Lock Status                                */
+/*                                Lock Status                           */
 /*----------------------------------------------------------------------*/
 void handleLockStatus(){
   digitalWrite(LED_BUILTIN, HIGH);
 }
 
 /*----------------------------------------------------------------------*/
-/*                                Battery Status                                */
+/*                                Battery Status                        */
 /*----------------------------------------------------------------------*/
 void handleBatteryStatus(){
   digitalWrite(LED_BUILTIN, HIGH);
@@ -398,7 +385,7 @@ void handleNotFound(){
 }
 
 /*----------------------------------------------------------------------*/
-/*                             handle not found                         */
+/*                              setSubscriber                           */
 /*----------------------------------------------------------------------*/
 void setSubscriber(){
   if(WiFi.status()== WL_CONNECTED)
@@ -407,17 +394,31 @@ void setSubscriber(){
     httpSubscriber.begin("http://api.backendless.com/8178E63C-1936-5C74-FFC6-B90735872B00/BA7F64EC-A219-CA3F-FFB7-B81B1BBC5300/messaging/toggle/subscribe");
     httpSubscriber.addHeader("Content-Type", "application/json");
     String postMessage = String("{\"subtopic\":\"123456789\"}");
-    int httpCode = httpSubscriber.POST(postMessage);
+    httpCode = httpSubscriber.POST(postMessage);
     String payload = httpSubscriber.getString();
     httpSubscriber.end();
 
     httpSubscriber.begin("http://api.backendless.com/8178E63C-1936-5C74-FFC6-B90735872B00/BA7F64EC-A219-CA3F-FFB7-B81B1BBC5300/messaging/toggle/" + payload.substring(19, 55));
     httpSubscriber.addHeader("Content-Type", "application/json");
     httpCode = httpSubscriber.GET();
-    String thisString = httpSubscriber.getString();
+    thisString = httpSubscriber.getString();
+    httpSubscriber.end();
     server.send(200, "text/plain", "200: Ok" + httpCode);
-    if (httpCode == 200 && thisString != "{\"messages\":[]}")
+    if(httpCode == 200 && thisString != "{\"messages\":[]}"){
+      dataready = true;
+    }
+    Serial.println("setSubscriber done!!!!!!!!!");
+  }
+   else
+  {
+    server.send(400, "text/plain", "400: error");
+  }
+}
+
+void handle_DataParser(){
+   if (httpCode == 200 && thisString != "{\"messages\":[]}")
     {
+      Serial.println("data parser is begin....");
       const size_t bufferSize = JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(1) + JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(5) + 310;
       DynamicJsonBuffer jsonBuffer(bufferSize);
       
@@ -432,36 +433,19 @@ void setSubscriber(){
       messages0.prettyPrintTo(g);
       Serial.println(g);
       String messages0_data = messages0["data"];
-      
-          if (messages0_data == "toggle")
+         if (messages0_data == "toggle")
         {
-         if(doorDirection == 0){
-          doorDirection = 1;
-    
-          digitalWrite(MotorPin1, LOW);
-          digitalWrite(MotorPin2, HIGH);
-          }
-          else{
-          doorDirection = 0;
-          
-          digitalWrite(MotorPin1, HIGH);
-          digitalWrite(MotorPin2, LOW);
-          }
-//          handleSaveStatusInServer();
-//          handlepublish();
+          handleToggle();
+          handleSaveStatusInServer();
+          handlepublish();
         }
+     String messageId_data = messages0["messageId"];
     }
-  }
-  else
-  {
-    server.send(400, "text/plain", "400: error");
-  }
-  httpSubscriber.end();
 }
 
 void handlepublish(){
   HTTPClient publishHttp;
-  publishHttp.begin("http://api.backendless.com/8178E63C-1936-5C74-FFC6-B90735872B00/BA7F64EC-A219-CA3F-FFB7-B81B1BBC5300/messaging/reponse_toggle");
+  publishHttp.begin("http://api.backendless.com/8178E63C-1936-5C74-FFC6-B90735872B00/BA7F64EC-A219-CA3F-FFB7-B81B1BBC5300/messaging/response_toggle");
   publishHttp.addHeader("Content-Type", "application/json");
   String postMessage = String("{\"message\":\"ok\", \"subtopic\":\"123456789\"}");
   int httpCode = publishHttp.POST(postMessage);
@@ -470,14 +454,20 @@ void handlepublish(){
 }
 
 void handleSaveStatusInServer(){
-  get_Status(&DoorData);
-  HTTPClient http;
-  http.begin("http://api.backendless.com/8178E63C-1936-5C74-FFC6-B90735872B00/BA7F64EC-A219-CA3F-FFB7-B81B1BBC5300/data/Lock/347F7393-36C8-543C-FFED-C363960EA700");
-  http.addHeader("Content-Type", "application/json");
-  String postMessage = String("{\"wifi_status\" :" + String(DoorData.wifi_status) + ",\"battery_status\": 3 ,\"lock_status\" : " + String(DoorData.lock_status) + ",\"door_status\" : " + String(DoorData.door_status) + ",\"connection_status\" : "+ String(DoorData.Connection_status) +"}");
-  int httpCode = http.PUT(postMessage);
-  String payload = http.getString();
-  http.end();
+  if(WiFi.status()== WL_CONNECTED){
+    Serial.println("SaveStatusInServer start....");
+    get_Status(&DoorData);
+    HTTPClient http;
+    http.begin("http://api.backendless.com/8178E63C-1936-5C74-FFC6-B90735872B00/BA7F64EC-A219-CA3F-FFB7-B81B1BBC5300/data/Lock/347F7393-36C8-543C-FFED-C363960EA700");
+    http.addHeader("Content-Type", "application/json");
+    Serial.println(DoorData.lock_status);
+    String postMessage = "{\"wifi_status\" :" + String(DoorData.wifi_status) + ",\"battery_status\": 3 ,\"lock_status\" : " + (DoorData.lock_status == 0?"false":"true") + ",\"door_status\" : " + (DoorData.door_status == 0?"false":"true") + ",\"connection_status\" : " + (DoorData.Connection_status == 0?"false":"true") +"}";
+    Serial.println(postMessage);
+    int httpCode = http.PUT(postMessage);
+    String payload = http.getString();
+    Serial.println("payload"+payload);
+    http.end();
+  }
 }
 /*----------------------------------------------------------------------------------------------------------------------------------------*/
 /*                                                            alternative function                                                        */
@@ -539,6 +529,7 @@ bool get_doorStatus(){
   else{
     doorStatus  = false;
   }
+  Serial.println(doorStatus);
   return(doorStatus);
 }
 
@@ -559,5 +550,44 @@ void get_Status(tys_Status *pvS){
   pvS->lock_status    = get_lockStatus();
   pvS->Connection_status = ConnectionStatus();
   pvS->door_status = get_doorStatus();
+}
+
+void send_ChangeStatus(){
+  get_Status(&DoorData);
+//  if(DoorData.battery_status != LastDoorData.battery_status){
+//    Status_RetValue = 1;
+//    Serial.println("battery_status Status change....");
+//  }
+
+  if(DoorData.wifi_status != LastDoorData.wifi_status){
+    Status_RetValue = 1;
+    Serial.println("wifi_status Status change....");
+  }
+
+  if(DoorData.lock_status != LastDoorData.lock_status){
+    Status_RetValue = 1;
+    Serial.println("lock_status Status change....");
+  }
+
+  if(DoorData.Connection_status != LastDoorData.Connection_status){
+    Status_RetValue = 1;
+    Serial.println("Connection_status Status change....");
+  }
+
+  if(DoorData.door_status != LastDoorData.door_status){
+    Status_RetValue = 1;
+    Serial.println("door_status Status change....");
+  }
+
+  if(Status_RetValue == 1){
+    handleSaveStatusInServer();
+    Status_RetValue = 0;
+    LastDoorData.battery_status    = DoorData.battery_status;
+    LastDoorData.wifi_status       = DoorData.wifi_status;
+    LastDoorData.lock_status       = DoorData.lock_status;
+    LastDoorData.door_status       = DoorData.door_status;
+    LastDoorData.Connection_status = DoorData.Connection_status;
+    Serial.println("Status change....");
+  }
 }
 
